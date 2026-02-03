3dd18381d2595181366115d7691a1fd988a8afbb
Fabrice KAYO <fabricekayo@MacBook-Air-de-Fabrice.local>
2026-02-03 06:08:03 +0100

    feat(reminders): add deferral and notification actions

--- lib/features/reminders/domain/notification_action_handler.dart
+++ lib/features/reminders/domain/notification_action_handler.dart
@@ -1,15 +1,19 @@
 import '../../contacts/domain/contact_call_action_service.dart';
 import '../../contacts/domain/contact_write_action_service.dart';
+import 'reminder_deferral_service.dart';
 
 class NotificationActionHandler {
   NotificationActionHandler({
     required ContactWriteActionService writeService,
     required ContactCallActionService callService,
+    required ReminderDeferralService deferralService,
   })  : _writeService = writeService,
-        _callService = callService;
+        _callService = callService,
+        _deferralService = deferralService;
 
   final ContactWriteActionService _writeService;
   final ContactCallActionService _callService;
+  final ReminderDeferralService _deferralService;
 
   Future<ContactWriteOutcome> handleWriteAction({
     required String contactId,
@@ -23,5 +27,12 @@ class NotificationActionHandler {
     required Uri uri,
   }) {
     return _callService.launchCall(contactId: contactId, uri: uri);
+  }
+
+  Future<String> handleDeferralAction({
+    required String contactId,
+    required ReminderDeferralType type,
+  }) {
+    return _deferralService.defer(contactId: contactId, type: type);
   }
 }
--- lib/features/reminders/domain/notification_action_payload.dart
+++ lib/features/reminders/domain/notification_action_payload.dart
@@ -1,3 +1,5 @@
+import 'reminder_deferral_service.dart';
+
 class NotificationWriteActionPayload {
   const NotificationWriteActionPayload({
     required this.contactId,
@@ -18,35 +20,67 @@ class NotificationCallActionPayload {
   final Uri uri;
 }
 
+class NotificationDeferActionPayload {
+  const NotificationDeferActionPayload({
+    required this.contactId,
+    required this.type,
+  });
+
+  final String contactId;
+  final ReminderDeferralType type;
+}
+
 class NotificationActionPayloadParser {
   const NotificationActionPayloadParser._();
 
   static Object? tryParse(Object? args) {
     if (args is NotificationWriteActionPayload ||
-        args is NotificationCallActionPayload) {
+        args is NotificationCallActionPayload ||
+        args is NotificationDeferActionPayload) {
       return args;
     }
     if (args is Map) {
       final action = args['action'] ?? args['type'];
       final contactId = args['contactId'] ?? args['contact_id'];
       final uriValue = args['uri'] ?? args['url'];
-      if (action is! String || contactId is! String || uriValue == null) {
-        return null;
-      }
-      final uri = uriValue is Uri ? uriValue : Uri.tryParse('$uriValue');
-      if (uri == null) {
+      if (action is! String || contactId is! String) {
         return null;
       }
       switch (action) {
         case 'write':
+          if (uriValue == null) {
+            return null;
+          }
+          final uri = uriValue is Uri ? uriValue : Uri.tryParse('$uriValue');
+          if (uri == null) {
+            return null;
+          }
           return NotificationWriteActionPayload(
             contactId: contactId,
             uri: uri,
           );
         case 'call':
+          if (uriValue == null) {
+            return null;
+          }
+          final uri = uriValue is Uri ? uriValue : Uri.tryParse('$uriValue');
+          if (uri == null || uri.scheme != 'tel' || uri.path.isEmpty) {
+            return null;
+          }
           return NotificationCallActionPayload(
             contactId: contactId,
             uri: uri,
+          );
+        case 'later':
+          return NotificationDeferActionPayload(
+            contactId: contactId,
+            type: ReminderDeferralType.later,
+          );
+        case 'not_now':
+        case 'not-now':
+          return NotificationDeferActionPayload(
+            contactId: contactId,
+            type: ReminderDeferralType.notNow,
           );
       }
     }
